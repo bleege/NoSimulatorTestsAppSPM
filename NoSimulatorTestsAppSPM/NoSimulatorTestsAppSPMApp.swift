9f534e65da5e89570a5c29b3d06806633317845e
Brad Leege <bleege@gmail.com>
2024-11-18 20:13:30 -0600

    Initial Commit

---
+++ NoSimulatorTestsAppSPM/NoSimulatorTestsAppSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSimulatorTestsAppSPMApp.swift
+//  NoSimulatorTestsAppSPM
+//
+//  Created by Brad Leege on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NoSimulatorTestsAppSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
