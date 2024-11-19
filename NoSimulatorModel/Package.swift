369b3a0ec9fe660d0b4a5b46ee54b15f351dca40
Brad Leege <bleege@gmail.com>
2024-11-18 22:36:17 -0600

    Data Model As Resource

--- NoSimulatorModel/Package.swift
+++ NoSimulatorModel/Package.swift
@@ -15,7 +15,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NoSimulatorModel"),
+            name: "NoSimulatorModel",
+            resources: [
+                .copy("Resources/NoSimulatorCoreDataModel.xcdatamodeld")
+            ]
+        ),
         .testTarget(
             name: "NoSimulatorModelTests",
             dependencies: ["NoSimulatorModel"]
