c50e8ed0d406247ea67570c1cad67d31c00bf095
Brad Leege <bleege@gmail.com>
2025-01-01 16:27:22 -0600

    ButtonTap Data Object

---
+++ NoSimulatorData/Sources/NoSimulatorData/ButtonTap.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonTap.swift
+//  NoSimulatorData
+//
+//  Created by Brad Leege on 1/1/25.
+//
+
+import Foundation
+
+struct ButtonTap: Codable {
+    
+    let id: UUID
+    let dateTapped: Date
+    
+}
--- NoSimulatorData/Tests/NoSimulatorDataTests/NoSimulatorDataTests.swift
+++ NoSimulatorData/Tests/NoSimulatorDataTests/NoSimulatorDataTests.swift
@@ -1,6 +1,27 @@
+import Foundation
 import Testing
 @testable import NoSimulatorData
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+
+struct NoSimulatorDataTests {
+
+    @Test func testButtonTapRoundtrip() async throws {
+        
+        let id = UUID()
+        let date = Date()
+        let buttonTap = ButtonTap(id: id, dateTapped: date)
+
+        let encoder = JSONEncoder()
+        let encodedButtonTap = try encoder.encode(buttonTap)
+        
+        #expect(encodedButtonTap != nil)
+        
+        let decoder = JSONDecoder()
+        let decodedButtonTap = try decoder.decode(ButtonTap.self, from: encodedButtonTap)
+
+        #expect(decodedButtonTap.id == id)
+        #expect(decodedButtonTap.dateTapped == date)
+            
+    }
+    
 }
